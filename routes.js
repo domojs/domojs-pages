1240dfd49178e31a14e1bfed83514898df09bc5e
npenin <nicolas.penin@dragon-angel.fr>
2016-01-26 06:39:44 +0100

    added home page for home screen dashboard

--- routes.js
+++ routes.js
@@ -4,3 +4,6 @@ route.on('remote', function(url, params, unchanged){
 route.on('shutters', function(url, params, unchanged){
     $.ajax(loadJsonFromUrl('/assets/pages/shutters.json')); 
 });
+route.on('home', function(url, params, unchanged){
+    $.ajax(loadHtml('/assets/pages/home.html')); 
+});
